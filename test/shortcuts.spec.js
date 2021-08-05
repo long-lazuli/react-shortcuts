ff5bc3c89b312e1179186f310d8245ff24a38862
Yannick Schetrite <y.schetrite@myunisoft.fr>
2021-08-05 09:50:49 +0200

    use react17

--- test/shortcuts.spec.js
+++ test/shortcuts.spec.js
@@ -5,7 +5,7 @@ import sinonChai from 'sinon-chai'
 import sinon from 'sinon'
 import _ from 'lodash'
 import enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 import keymap from './keymap'
 
 enzyme.configure({ adapter: new Adapter() })
